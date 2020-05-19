ce8a8f14e8a43d0f6a8b4dd5f7bb72ed5fceb495
Jasmin Frei <jasmin.frei@hotmail.com>
2020-05-19 09:57:10 +0200

    feat: use asyncstorage

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -31,56 +31,12 @@ let decks = {
     }
 }
 
-function generateUID() {
+export const DECKS_STORAGE_KEY = "MobileFlashcard:decks"
+
+export function generateUID() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
 
-export function _getDecks() {
-    return new Promise((res, rej) => {
-        setTimeout(() => res({...decks}), 1000);
-    })
-}
-
-export function _getDeck(id) {
-    return new Promise((res, rej) => {
-        setTimeout(() => res(decks[id]), 1000);
-    })
-}
-
-export function _saveDeck(title) {
-    return new Promise((res, rej) => {
-        let id = generateUID();
-        setTimeout(() => {
-            decks = {
-                ...decks,
-                [id]: {
-                    title,
-                    cards: []
-                }
-            }
-            res();
-        }, 1000)
-    })
-}
-
-export function _saveCard({deckId, question, answer}) {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            decks = {
-                ...decks,
-                [deckId]: {
-                    ...decks[deckId],
-                    cards: [
-                        ...decks[deckId].cards,
-                        {
-                            question,
-                            answer
-                        }
-                    ]
-                }
-            }
-        }, 1000);
-
-        res();
-    })
+export function _getInitialData() {
+    return decks;
 }
--- utils/api.js
+++ utils/api.js
@@ -1,17 +1,113 @@
-import { _getDeck, _getDecks, _saveCard, _saveDeck } from "./_DATA";
+
+import { _getInitialData, DECKS_STORAGE_KEY, generateUID } from "./_DATA";
+import AsyncStorage from '@react-native-community/async-storage'
 
 export function getAllDecks() {
-    return _getDecks();
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then(checkDeckInitialization);
+}
+
+function checkDeckInitialization(result) {
+    if (result === null) {
+        let initialData = _getInitialData();
+        AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(initialData));
+        return initialData;
+    }
+    return result;
 }
 
 export function getDeck(id) {
-    return _getDeck(id);
+    return getAllDecks().then((decks) => decks[id]);
 }
 
 export function saveDeck(title) {
-    return _saveDeck(title);
+    let id = generateUID();
+    return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [id]: {
+            title
+        }
+    }))
 }
 
 export function saveCard(deckId, question, answer) {
-    return _saveCard({deckId, question, answer});
+    return getDeck(deckId).then((deck) => AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [deckId]: {
+            cards: [...deck.cards, {
+                question,
+                answer
+            }]
+        }
+    })))    
 }
+
+/* 
+
+export function submitEntry({ entry, key }) {
+    console.log('tat ', key)
+    return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
+        [key]: entry
+    }));
+}
+
+export function removeEntry(key) {
+    return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            data[key] = undefined;
+            delete data[key];
+            AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+        });
+} 
+
+export function _getDecks() {
+    return new Promise((res, rej) => {
+        setTimeout(() => res({...decks}), 1000);
+    })
+}
+
+export function _getDeck(id) {
+    return new Promise((res, rej) => {
+        setTimeout(() => res(decks[id]), 1000);
+    })
+}
+
+export function _saveDeck(title) {
+    return new Promise((res, rej) => {
+        let id = generateUID();
+        setTimeout(() => {
+            decks = {
+                ...decks,
+                [id]: {
+                    title,
+                    cards: []
+                }
+            }
+            res();
+        }, 1000)
+    })
+}
+
+export function _saveCard({deckId, question, answer}) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            decks = {
+                ...decks,
+                [deckId]: {
+                    ...decks[deckId],
+                    cards: [
+                        ...decks[deckId].cards,
+                        {
+                            question,
+                            answer
+                        }
+                    ]
+                }
+            }
+        }, 1000);
+
+        res();
+    })
+}
+
+
+*/
